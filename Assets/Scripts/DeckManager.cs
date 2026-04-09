69a69f2563ec3d6ea5b8eccd9b4f9b99a10ca0a9
Kyle Angeles <kyle.angeles@dcmail.ca>
2026-04-08 20:28:01 -0400

    added the necessary scripts that i kinda want to start working on maybe tonight or tomorrow morning

---
+++ Assets/Scripts/DeckManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+// Author: Kyle Angeles, D'Andre, Karim, Alex
+// Date-Written 2026/4/8
+// File: DeckManager.cs
+// Description: Deck Manager is responsible for managing player's deck's in the decks screen
+// This includes adding cards to the deck, deleting decks, saving decks
+// Responsible for selecting decks on the second page of the deck screen and loading them into the game when the 
+// player clicks start
+
+// We need JSON Connectivity for this script to save and load decks from the game into the player's screen
+public class DeckManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
